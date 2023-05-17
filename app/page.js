30610425717eed8fda92a31d16ac66c6333d542b
Kalyan-Saxena <kalyansaxena123@gmail.com>
2023-05-17 18:49:00 +0530

    add feature branch changes

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          Welcome to the nextjs from feature-a branch&nbsp;
           <code className={styles.code}>app/page.js</code>
         </p>
         <div>
